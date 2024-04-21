046a797268fa56e72be2322d20c925f36cbbc274
Gaurav Sharma <gsharma1154@gmail.com>
2024-04-21 22:15:33 +0530

    add product-getImageByProductId-Chapter2

---
+++ src/main/java/com/telusko/ECommerceAppAddProductGetImageByProductIdApplication.java
@@ -0,0 +1,13 @@
+package com.telusko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceAppAddProductGetImageByProductIdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceAppAddProductGetImageByProductIdApplication.class, args);
+    }
+
+}
